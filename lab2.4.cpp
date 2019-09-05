1668d6dcbbf5fc46fdb463137d062575f930dafb
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 22:26:15 +0300

    Create lab2.4.cpp

---
+++ lab2.4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() 
+{
+    cout<<"Vvedite tochki"<<endl;
+    int x1,x2,y1,y2,x,y,s,p;
+    cin>>x1>>y1>>x2>>y2;
+    x=abs(x1-x2);
+    y=abs(y1-y2);
+    s=x*y;
+    p=(x+y)*2;
+    cout<<"P="<<p<<", S="<<s<<endl;
+}
