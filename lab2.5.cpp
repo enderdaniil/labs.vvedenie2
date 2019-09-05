21d159df93037d1d9390c2ba3b7254ed9ff64b89
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-05 22:27:26 +0300

    Create lab2.5.cpp

---
+++ lab2.5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    cout<<"Vvedite tochki"<<endl;
+    int x1,x2,x3,y1,y2,y3;
+    float s,p,a,b,c;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+    a=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    b=sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3));
+    c=sqrt((x3-x2)*(x3-x2)+(y3-y2)*(y3-y2));
+    p=a+b+c;
+    s=sqrt(p/2*(p/2-a)*(p/2-b)*(p/2-c));
+    cout<<"S="<<s<<", P="<<p;
+}
