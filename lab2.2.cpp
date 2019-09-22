4e7f931f348de202b85bb341eb5e55bc13967dd3
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-09-22 20:56:39 +0300

    Update lab2.2.cpp

--- lab2.2.cpp
+++ lab2.2.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main() 
 {
     cout<<"Введите координаты точек"<<endl;
     int a,b,c,ac,bc,sum;
     cin>>a>>b>>c;
-    if (a>c) {
-        ac=a-c;
-    }
-    else {ac=c-a;}
-    if (c>b) {
-        bc=c-b;
-    }
-    else {bc=c-b;}
+    ac=abs(a-c);
+    bc=abs(c-b);
     sum=ac+bc;
     cout<<"AC="<<ac<<", BC="<<bc<<", SUMMA="<<sum<<endl;
     return 0;
